7c3f78b3ccc9a448d544f62555c4247d9e67be39
Emanuele <emanuoro@gmail.com>
2024-01-27 11:13:27 +0100

    transcription script
    Transcription script and notify

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,21 +1,32 @@
 #!/bin/python3
-# script per scrittore dal microfono
 
 # Python program to translate
 # speech to text and text to speech
 
-
-# script funzionante solo in inglese
 import speech_recognition as sr
 import pyautogui
+from notifypy import Notify
+import os
 import time
 
-# Initialize the recognizer 
+
+# notification system
+def notification(which):
+	noti = Notify()
+	if which == 1:
+		noti.title = "Speech-to-Text"
+		noti.message = "Speak Anythings:"
+	if which == 0:
+		noti.title = "Speech-to-Text"
+		noti.message = "Sorry could not recognize what you said"
+	noti.send()
+		
+# listening and transcription system
 r = sr.Recognizer()
 x, y = pyautogui.position()
 
 with sr.Microphone() as source:
-	print("Speak Anythings: ")
+	notification(1)
 	r.adjust_for_ambient_noise(source)
 	audio = r.listen(source, timeout=30)
 	try:
@@ -24,4 +35,4 @@ with sr.Microphone() as source:
 		pyautogui.click(x, y)
 		pyautogui.typewrite(text)
 	except:
-		print("Sorry could not recognize what you said")
+		notification(0)
